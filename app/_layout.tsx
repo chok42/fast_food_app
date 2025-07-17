3f39014c17f1c4f3a217e0748c59fb07e3c08203
Sathaporn Bunkoed <sathaporn.chok42@gmail.com>
2025-07-17 10:44:15 +0700

    Set up the base file & folder structure for the rest of the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,24 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import './global.css';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [fontsLoaded,error] = useFonts({
+    "PlaypenSansThai-Thin": require("../assets/fonts/PlaypenSansThai-Thin.ttf"),
+    "PlaypenSansThai-ExtraLight": require("../assets/fonts/PlaypenSansThai-ExtraLight.ttf"),
+    "PlaypenSansThai-Light": require("../assets/fonts/PlaypenSansThai-Light.ttf"),
+    "PlaypenSansThai-Regular": require("../assets/fonts/PlaypenSansThai-Regular.ttf"),
+    "PlaypenSansThai-Medium": require("../assets/fonts/PlaypenSansThai-Medium.ttf"),
+    "PlaypenSansThai-SemiBold": require("../assets/fonts/PlaypenSansThai-SemiBold.ttf"),
+    "PlaypenSansThai-Bold": require("../assets/fonts/PlaypenSansThai-Bold.ttf"),
+    "PlaypenSansThai-ExtraBold": require("../assets/fonts/PlaypenSansThai-ExtraBold.ttf"),
+  })
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  useEffect(() => {
+     if(error) throw  error;
+     if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded,error])
+  
+  return <Stack  screenOptions={{headerShown:false}}/>;
 }
