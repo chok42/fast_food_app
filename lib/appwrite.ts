cc8e92153bb6c57540e7572a8a81a3e497df0821
Sathaporn Bunkoed <sathaporn.chok42@gmail.com>
2025-07-17 13:56:20 +0700

    Set up the base file & folder structure for the rest of the app

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.jsc.fastfood",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '687878ca0015785ae56a',
+    userCollectionId:"6878797e000afda58fef"
+}
